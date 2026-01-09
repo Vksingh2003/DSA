e26001b8bddc35936565624292422a6bd7d32905
vivek kumar singh <119318184+Vksingh2003@users.noreply.github.com>
2026-01-09 23:05:13 +0530

    Time: 3 ms (62.46%) | Memory: 47.5 MB (79.29%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int repeat = -1, missing = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            int idx = Math.abs(nums[i]) - 1;
+            if (nums[idx] < 0) {
+                repeat = idx + 1;
+            } else {
+                nums[idx] = -nums[idx];
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                missing = i + 1;
+            }
+        }
+
+        return new int[]{repeat, missing};
+    }
+}
