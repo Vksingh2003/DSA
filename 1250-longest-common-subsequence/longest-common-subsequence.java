b85598bd2669245fd8f0c31db243ae64365f1763
vivek kumar singh <119318184+Vksingh2003@users.noreply.github.com>
2026-07-16 22:20:50 +0530

    Time: 7 ms (99.99%) | Memory: 43 MB (95.83%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,26 +1,19 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         if (text1.length() < text2.length()) {
-            return LCS(text1, text2);
+            return longestCommonSubsequence(text2, text1);
         }
-        return LCS(text2, text1);
-    }
-
-    public int LCS(String s1, String s2) {
-        int[][] M = new int[2][s1.length() + 1];
-
-        for (int i = 1; i <= s2.length(); i++) {
-            M[i % 2][0] = 0;
-            for (int j = 1; j <= s1.length(); j++) {
-                if (s1.charAt(j - 1) == s2.charAt(i - 1)) {
-                    M[i % 2][j] = M[(i - 1) % 2][j - 1] + 1;
-                } else {
-                    M[i % 2][j] = Math.max(M[(i - 1) % 2][j - 1],
-                            Math.max(M[(i - 1) % 2][j], M[i % 2][j - 1]));
-                }
+        int[] dp = new int[text2.length() + 1];
+        char[] ch1 = text1.toCharArray();
+        char[] ch2 = text2.toCharArray();
+        for (char c1 : ch1) {
+            int prevDiag = 0;
+            for (int j = 1; j < dp.length; j++) {
+                int tmp = dp[j];
+                dp[j] = c1 == ch2[j - 1] ? prevDiag + 1 : Math.max(dp[j], dp[j - 1]);
+                prevDiag = tmp;
             }
         }
-        return M[s2.length() % 2][s1.length()];
+        return dp[dp.length - 1];
     }
 }
-
