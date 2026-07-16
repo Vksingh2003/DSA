575f36de3740f8dd20eb2ac2392654914da425ec
vivek kumar singh <119318184+Vksingh2003@users.noreply.github.com>
2026-07-16 21:47:36 +0530

    Time: 11 ms (99.14%) | Memory: 18.7 MB (5.50%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.cpp
@@ -1,41 +1,82 @@
 class Solution {
 public:
-    pair<int, int> solveMem(vector<int>& nums, int curr, int prev, vector<vector<pair<int, int>>>& dp) {
-        if (curr == nums.size()) return make_pair(0, 1);
-
-        if (dp[curr][prev + 1].first != -1) return dp[curr][prev + 1];
-
-        int length = 0, count = 0;
-
-        if (prev == -1 || nums[prev] < nums[curr]){
-            pair<int, int> include = solveMem(nums, curr + 1, curr, dp);
-            int includeLength = 1 + include.first;
-            if (includeLength > length){
-                length = includeLength;
-                count = include.second;
-            } 
-            else if (includeLength == length){
-                count += include.second;
+    struct Node {
+        int len = 0, cnt = 0;
+    };
+    class Bit
+    {
+        public:
+        int n;
+        vector<Node> bit;
+        Bit(int sizeOfBit)
+        {
+            n = sizeOfBit;
+            bit.resize(n+1);
+            for(int i=0; i<=n; ++i)
+            {
+                Node node;
+                bit[i] = node;
             }
         }
 
-        pair<int, int> exclude = solveMem(nums, curr + 1, prev, dp);
-        if (exclude.first > length){
-            length = exclude.first;
-            count = exclude.second;
-        } 
-        else if (exclude.first == length){
-            count += exclude.second;
+        Node Merge(Node a, Node b)
+        {
+            if(a.len == 0 || a.len < b.len)
+                return b;
+            if(b.len == 0 || b.len < a.len)
+                return a;
+            Node res;
+            res.len = a.len;
+            res.cnt = a.cnt + b.cnt;
+            return res;
         }
 
-        dp[curr][prev + 1] = make_pair(length, count);
+        void Update(int idx, Node node)
+        {
+            while(idx <= n)
+            {
+                bit[idx] = Merge(bit[idx], node);
+                idx += (idx & (-idx));
+            }
+        }
 
-        return dp[curr][prev + 1];
-    }
+        Node Query(int idx)
+        {
+            Node node;
+            while(idx > 0)
+            {
+                node = Merge(node, bit[idx]);
+                idx -= (idx & (-idx));
+            }
+            return node;
+        }
+    };
     int findNumberOfLIS(vector<int>& nums) {
-        int curr = 0, prev = -1;
-        vector<vector<pair<int, int>>> dp(nums.size(), vector<pair<int, int>>(nums.size() + 1, make_pair(-1, -1)));
-        pair<int, int> result = solveMem(nums, curr, prev, dp);
-        return result.second;
+        // sorting part
+        vector<int> arr = nums;
+        sort(arr.begin(), arr.end());
+
+        // index map part
+        unordered_map<int, int> mp;
+        int j=1;
+        for(auto x : arr)
+            mp[x] = j++;
+        
+        Bit bit(nums.size());
+
+        Node res;
+        for(auto x : nums)
+        {
+            int idx = mp[x];
+            Node cur = bit.Query(idx-1);
+            if(cur.len == 0 && cur.cnt == 0)
+                cur.len = cur.cnt = 1;
+            else
+                cur.len += 1;
+            res = bit.Merge(res, cur);
+            bit.Update(idx, cur);
+        }
+
+        return res.cnt;
     }
 };
